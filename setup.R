121f2d370d5cb4205ae99abbc38304c4974d888c
Arkadiusz Gładki <arek@dobio.link>
2026-06-16 22:14:54 +0200

    Add ggpubr to cran_pkgs in setup.R

--- setup.R
+++ setup.R
@@ -24,7 +24,7 @@ bioc_pkgs <- c(
 
 cran_pkgs <- c(
   "data.table", "ggplot2", "purrr", "qs2",
-  "summarytools", "writexl", "testthat", "svglite"
+  "summarytools", "writexl", "testthat", "svglite", "ggpubr"
 )
 
 options(Ncpus = 1)
